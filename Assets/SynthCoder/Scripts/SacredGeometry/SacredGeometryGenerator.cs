e848c3ae27f2b67e0d304ecc97204169c0330a7e
Chris Oje <chroje@microsoft.com>
2023-02-19 20:32:18 -0800

    added pyramid

--- Assets/SynthCoder/Scripts/SacredGeometry/SacredGeometryGenerator.cs
+++ Assets/SynthCoder/Scripts/SacredGeometry/SacredGeometryGenerator.cs
@@ -55,7 +55,7 @@ public class SacredGeometryGenerator : MonoBehaviour
                 meshFilter.mesh = GenerateCapsuleMesh(size);
                 break;
             case ShapeType.Pyramid:
-                meshFilter.mesh = GeneratePyramidMesh(size);
+                meshFilter.mesh = GeneratePyramidMesh(size, size);  //TODO: Height param
                 break;
             case ShapeType.StarTetrahedron:
                 meshFilter.mesh = GenerateStarTetrahedronMesh(size);
@@ -118,10 +118,66 @@ public class SacredGeometryGenerator : MonoBehaviour
         return null;
     }
 
-    public static Mesh GeneratePyramidMesh(float size)
+    public static Mesh GeneratePyramidMesh(float baseSize, float height)
     {
-        //TODO: Implement pyramid mesh generation
-        return null;
+        var mesh = new Mesh();
+
+        // Define vertices of the pyramid
+        var vertices = new Vector3[5];
+        vertices[0] = new Vector3(0f, height, 0f);                          // Apex
+        vertices[1] = new Vector3(-baseSize / 2f, 0f, baseSize / 2f);       // Front left corner
+        vertices[2] = new Vector3(baseSize / 2f, 0f, baseSize / 2f);        // Front right corner
+        vertices[3] = new Vector3(baseSize / 2f, 0f, -baseSize / 2f);       // Back right corner
+        vertices[4] = new Vector3(-baseSize / 2f, 0f, -baseSize / 2f);      // Back left corner
+
+        // Define triangles of the pyramid
+        var triangles = new int[18];
+        // Front face
+        triangles[0] = 0; triangles[1] = 1; triangles[2] = 2;
+        // Right face
+        triangles[3] = 0; triangles[4] = 2; triangles[5] = 3;
+        // Back face
+        triangles[6] = 0; triangles[7] = 3; triangles[8] = 4;
+        // Left face
+        triangles[9] = 0; triangles[10] = 4; triangles[11] = 1;
+        // Bottom face
+        triangles[12] = 4; triangles[13] = 3; triangles[14] = 2;
+        triangles[15] = 4; triangles[16] = 2; triangles[17] = 1;
+
+        // Define UVs for texturing the mesh
+        var uvs = new Vector2[5];
+        uvs[0] = new Vector2(0.5f, 1f);     // Apex
+        uvs[1] = new Vector2(0f, 0f);       // Front left corner
+        uvs[2] = new Vector2(1f, 0f);       // Front right corner
+        uvs[3] = new Vector2(1f, 1f);       // Back right corner
+        uvs[4] = new Vector2(0f, 1f);       // Back left corner
+
+        // Define normals for lighting the mesh
+        var normals = new Vector3[5];
+        normals[0] = Vector3.up;            // Apex
+        normals[1] = Vector3.up;            // Front left corner
+        normals[2] = Vector3.up;            // Front right corner
+        normals[3] = Vector3.up;            // Back right corner
+        normals[4] = Vector3.up;            // Back left corner
+
+        // Set up the mesh with the defined vertices, triangles, UVs, and normals
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.uv = uvs;
+        mesh.normals = normals;
+
+        // Scale the mesh to the given height
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i].y *= height;
+        }
+        mesh.vertices = vertices;
+
+        // Recalculate the bounds and the normals of the mesh
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+
+        return mesh;
     }
 
     public static Mesh GenerateStarTetrahedronMesh(float size)
@@ -132,28 +188,31 @@ public class SacredGeometryGenerator : MonoBehaviour
         float root3 = Mathf.Sqrt(3f);
         float root6 = Mathf.Sqrt(6f);
 
-        Vector3[] vertices = new Vector3[] {
-            new Vector3(0f, 0f, size / root3), // Apex
-            new Vector3(size / root2, 0f, -size / (2f * root3)), // Base vertex 1
-            new Vector3(-size / (2f * root2), size / 2f, -size / (2f * root3)), // Base vertex 2
-            new Vector3(-size / (2f * root2), -size / 2f, -size / (2f * root3)) // Base vertex 3
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3(0f, size / (2f * root3), 0f), // Apex
+            new Vector3(-size / (2f * root2), 0f, size / (2f * root3)), // Base vertex 1
+            new Vector3(size / (2f * root2), 0f, size / (2f * root3)), // Base vertex 2
+            new Vector3(0f, -size / root3, 0f) // Base vertex 3
         };
 
-        int[] triangles = new int[] {
-            0, 1, 2, // Bottom pyramid
-            0, 2, 3,
-            0, 3, 1,
-            1, 2, 3, // Top pyramid
+        int[] triangles = new int[]
+        {
+            0, 2, 1, // Bottom pyramid
+            0, 1, 3,
+            1, 2, 3, // Front pyramid
+            0, 3, 2 // Back pyramid
         };
 
-        Vector3[] normals = new Vector3[] {
-            -Vector3.forward, -Vector3.forward, -Vector3.forward, // Bottom pyramid
-            Vector3.forward, Vector3.forward, Vector3.forward // Top pyramid
+        Vector3[] normals = new Vector3[]
+        {
+            Vector3.up, Vector3.up, Vector3.up, Vector3.up // All faces point up
         };
 
         mesh.vertices = vertices;
         mesh.triangles = triangles;
         mesh.normals = normals;
+        mesh.RecalculateBounds();
 
         return mesh;
     }
