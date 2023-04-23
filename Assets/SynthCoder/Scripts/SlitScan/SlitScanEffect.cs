dcd92474026c3407dcd5a7f730b9be04e699c4aa
David J. Calabrese <david_j_calabrese@hotmail.com>
2023-04-22 21:32:32 -0700

    Initial slit scan effect
    Adding shaders, scripts, prefabs, and scene updates.

---
+++ Assets/SynthCoder/Scripts/SlitScan/SlitScanEffect.cs
@@ -0,0 +1,62 @@
+// Code synthesized by SynthCoder, an AI language model trained by OpenAI.
+// While I may not have a physical form, I take pride in the work I create
+// and hope it helps make your programming journey a little easier.
+// Stay curious and keep coding!
+
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
+public class SlitScanEffect : MonoBehaviour
+{
+    public Material material;
+    public int segments = 64;
+    public float radius = 1f;
+    public float length = 1f;
+
+    void Start()
+    {
+        Mesh mesh = new Mesh();
+
+        Vector3[] vertices = new Vector3[(segments + 1) * 2];
+        Vector2[] uv = new Vector2[(segments + 1) * 2];
+        int[] triangles = new int[segments * 6];
+
+        for (int i = 0; i <= segments; i++)
+        {
+            float angle = i / (float)segments * Mathf.PI * 2f;
+
+            vertices[i * 2] = new Vector3(Mathf.Cos(angle) * radius, 0f, Mathf.Sin(angle) * radius);
+            vertices[i * 2 + 1] = new Vector3(Mathf.Cos(angle) * radius, length, Mathf.Sin(angle) * radius);
+
+            uv[i * 2] = new Vector2(i / (float)segments, 0f);
+            uv[i * 2 + 1] = new Vector2(i / (float)segments, 1f);
+        }
+
+        for (int i = 0; i < segments; i++)
+        {
+            triangles[i * 6] = i * 2;
+            triangles[i * 6 + 1] = i * 2 + 3;
+            triangles[i * 6 + 2] = i * 2 + 1;
+
+            triangles[i * 6 + 3] = i * 2;
+            triangles[i * 6 + 4] = i * 2 + 2;
+            triangles[i * 6 + 5] = i * 2 + 3;
+        }
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+
+        Vector3[] normals = mesh.normals;
+        for (int i = 0; i < normals.Length; i++)
+        {
+            normals[i] = -normals[i];
+        }
+        mesh.normals = normals;
+
+        GetComponent<MeshFilter>().mesh = mesh;
+        GetComponent<MeshRenderer>().material = material;
+    }
+}
