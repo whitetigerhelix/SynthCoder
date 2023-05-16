3dd097e9445e338dc18a49da799e14c45f89a9ee
Chris Oje <chroje@microsoft.com>
2023-05-16 10:58:39 -0700

    tornado spiral fix

--- Assets/SynthCoder/Scripts/Spirals/TornadoSpiral.cs
+++ Assets/SynthCoder/Scripts/Spirals/TornadoSpiral.cs
@@ -10,10 +10,14 @@ namespace SynthCoder
     public class TornadoSpiral : MonoBehaviour
     {
         [SerializeField] private int numSpheres = 50;
+		public int NumSpheres { get => numSpheres; set => numSpheres = value; }
         [SerializeField] private float radius = 1.0f;
-        [SerializeField] private float height = 2.0f;
-        [SerializeField] private float speed = 1.0f;
-        [SerializeField] private Color colorA = Color.red;
+		public float Radius { get => radius; set => radius = value; }
+		[SerializeField] private float height = 2.0f;
+		public float Height { get => height; set => height = value; }
+		[SerializeField] private float speed = 1.0f;
+		public float Speed { get => speed; set => speed = value; }
+		[SerializeField] private Color colorA = Color.red;
         [SerializeField] private Color colorB = Color.blue;
 
         private GameObject[] spheres;
@@ -22,17 +26,17 @@ namespace SynthCoder
 
         private void Start()
         {
-            spheres = new GameObject[numSpheres];
+            spheres = new GameObject[NumSpheres];
 
             increment = Mathf.PI * (3.0f - Mathf.Sqrt(5.0f));
             phase = 0.0f;
 
-            for (int i = 0; i < numSpheres; i++)
+            for (int i = 0; i < NumSpheres; i++)
             {
                 GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
                 sphere.transform.parent = transform;
                 sphere.transform.localScale = Vector3.one * 0.2f;
-                sphere.GetComponent<Renderer>().material.color = Color.Lerp(colorA, colorB, (float)i / numSpheres);
+                sphere.GetComponent<Renderer>().material.color = Color.Lerp(colorA, colorB, (float)i / NumSpheres);
                 sphere.GetComponent<SphereCollider>().enabled = false;
                 spheres[i] = sphere;
             }
@@ -40,16 +44,16 @@ namespace SynthCoder
 
         private void Update()
         {
-            phase += Time.deltaTime * speed;
+            phase += Time.deltaTime * Speed;
 
-            for (int i = 0; i < numSpheres; i++)
+            for (int i = 0; i < NumSpheres; i++)
             {
-                float y = ((i * height) / numSpheres) - (height / 2.0f);
-                float r = Mathf.Sqrt(i / (float)numSpheres);
+                float y = ((i * Height) / NumSpheres) - (Height / 2.0f);
+                float r = Mathf.Sqrt(i / (float)NumSpheres);
                 float theta = i * increment + phase;
 
-                float x = r * Mathf.Cos(theta) * radius;
-                float z = r * Mathf.Sin(theta) * radius;
+                float x = r * Mathf.Cos(theta) * Radius;
+                float z = r * Mathf.Sin(theta) * Radius;
 
                 spheres[i].transform.localPosition = new Vector3(x, y, z);
             }
